e87fe19aeec3d806235ef60c83b2763af5ddd3ad
urgnj92 <urarat.0507@gmail.com>
2024-02-29 15:34:38 +0900

    0229_2

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
       }),
     ],
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/product-frontend/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   outputDir: 'dist'
 });
 
